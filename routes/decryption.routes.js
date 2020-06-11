53c8223100147d3ca455a91d7fbf6de2a24cddf7
Abubakar Sadiq Hassan <abubakarhassan59@gmail.com>
2020-06-11 14:32:05 +0100

    Added encryption and decryption routes

---
+++ routes/decryption.routes.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const router = express.Router()
+const objectController = require('../controllers/object.controller')
+// const textController = require('../controllers/text.controller')
+
+router
+    .route('/object')
+    .post(objectController.decryptObject)
+
+// router
+//     .route('/text')
+//     .post(textController)
+
+module.exports = router
